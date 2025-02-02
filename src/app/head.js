94d19251a9b35843bd0d2c60215ca04b0402cd19
natthapongpho <natthapongpho@allnow.co.th>
2025-02-02 23:05:00 +0700

    Fix goto quiz for student

---
+++ src/app/head.js
@@ -0,0 +1,14 @@
+export default function Head() {
+    return (
+        <>
+            <title>Nan Yang Texttile Group</title>
+            <meta property="og:url" content="https://nanyang-ss9w.vercel.app/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="ดูผลคะแนนของคุณ!" />
+            <meta property="og:description" content="ECO Quiz! มาดูกันว่าคุณจะได้เท่าไหร่!" />
+            <meta property="og:image" content="https://nanyang-ss9w.vercel.app/image/score1.png" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
+        </>
+    );
+}
