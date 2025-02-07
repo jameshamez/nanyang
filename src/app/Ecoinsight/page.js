cdd72752457c196e9bacd6e9c5460059fa37dbfb
Jinnawat.Jid <jinnawat.jid@outlook.com>
2025-02-07 12:01:18 +0700

    adjust polling time

--- src/app/Ecoinsight/page.js
+++ src/app/Ecoinsight/page.js
@@ -41,8 +41,8 @@ export default function GroupsPage() {
     // Initial fetch
     fetchGroups();
 
-    // Set up polling every 5 seconds
-    const intervalId = setInterval(fetchGroups, 5000);
+    // Set up polling every 20 seconds
+    const intervalId = setInterval(fetchGroups, 20000);
 
     // Clean up the interval on component unmount
     return () => clearInterval(intervalId);
